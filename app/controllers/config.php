9e2d990a3704c33938b1ad9f2072bef1d3ce078d
sangnguyen0302 <sangnguyen.030202@gmail.com>
2022-06-03 13:19:53 +0700

    b11

--- app/controllers/config.php
+++ app/controllers/config.php
@@ -1,3 +1,3 @@
 <?php
-define('APP_ROOT', dirname(dirname(__FILE__)));
+define('APP_ROOT', dir dirname(dirname(__FILE__)));
 ?>
--- app/views/home.php
+++ app/views/home.php
@@ -6,6 +6,7 @@
     <title>Ecommerce</title>
 </head>
 <body>
+    <h1> hihih</h1>
     <div class="header">
         this is header
     </div>
