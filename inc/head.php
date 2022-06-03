ce95c058836445ccd4a11499bc3204e964c63d33
sangnguyen0302 <sangnguyen.030202@gmail.com>
2022-06-03 19:12:11 +0700

    xong giao dien form

--- inc/head.php
+++ inc/head.php
@@ -3,6 +3,5 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- <link rel="stylesheet" href="<?= URL_ROOT ?>/css/style.css"> -->
-    <title>Ecommerce</title>
-</head>
+    <link type="text/css" rel="stylesheet" href= "../../css/style.css">
+
