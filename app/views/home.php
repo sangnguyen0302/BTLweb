079b7aee50cee26bdc6b540bb001a52160c58552
trickstars <nguyenminhkhoe123@gmail.com>
2022-06-18 02:11:51 +0700

    5-carousel

--- app/views/home.php
+++ app/views/home.php
@@ -14,20 +14,28 @@
         <div id="thecarousel" class="carousel slide" data-bs-ride="carousel">
             <div class="carousel-indicators">
                 <button type="button" data-bs-target="#thecarousel" data-bs-slide-to="0" class="active"></button>
-                <!--button type="button" data-bs-target="#thecarousel" data-bs-slide-to="1"></button>
-                <button type="button" data-bs-target="#thecarousel" data-bs-slide-to="2"></button-->
+                <button type="button" data-bs-target="#thecarousel" data-bs-slide-to="1"></button>
+                <button type="button" data-bs-target="#thecarousel" data-bs-slide-to="2"></button>
+                <button type="button" data-bs-target="#thecarousel" data-bs-slide-to="3"></button>
+                <button type="button" data-bs-target="#thecarousel" data-bs-slide-to="4"></button>
             </div>
 
             <div class="carousel-inner">
                 <div class="carousel-item active">
-                    <img src="https://t.ex-cdn.com/mgn.vn/resize/600x315/files/news/2022/05/19/spy-x-family-anya-minh-chung-cua-cau-con-nha-tong-khong-giong-long-cung-giong-canh-085605.jpg" alt="First slide" class="d-block w-100">
-                </div>
-                <!--div class="carousel-item">
-                    <img src="" alt="Second slide" class="d-block w-100">
+                    <img src="https://img.freepik.com/free-vector/kindergarten-school-scene-with-two-children-playing-toys-room_1308-61743.jpg?t=st=1655493033~exp=1655493633~hmac=855248686d9e7b0cd04b5c24413d1a1adb442dd3fded21b1a4653b3ef936dd40&w=996" alt="First slide" class="d-block w-100" height="500px">
                 </div>
                 <div class="carousel-item">
-                    <img src="" alt="Third slide" class="d-block w-100">
-                </div-->
+                    <img src="https://putatu.com/storage/campaign/f774561620f3dcf730d66fffd820d278.png" alt="Second slide" class="d-block w-100" height="500px">
+                </div>
+                <div class="carousel-item">
+                    <img src="https://i.pinimg.com/originals/60/37/ed/6037ed9add0170c6d368caa182dd7b22.png" alt="Third slide" class="d-block w-100" height="500px">
+                </div>
+                <div class="carousel-item">
+                    <img src="https://i.pinimg.com/originals/7f/1f/bb/7f1fbbdf63ab5b49e50dc0de5441fab7.png" alt="Third slide" class="d-block w-100" height="500px">
+                </div>
+                <div class="carousel-item">
+                    <img src="https://u6wdnj9wggobj.vcdn.cloud/media/mgs_blog/s/i/sinh-nhat-mykingdom-1004.jpg" alt="Third slide" class="d-block w-100" height="500px">
+                </div>
             </div>
 
             <button type="button" class="carousel-control-prev" data-bs-target="#thecarousel" data-bs-slide="prev">
