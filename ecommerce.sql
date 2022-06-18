ca97a1bf2e742abcd4c2a07fce14f098a390618b
sangnguyen0302 <sangnguyen.030202@gmail.com>
2022-06-18 23:53:40 +0700

    .

--- ecommerce.sql
+++ ecommerce.sql
@@ -1,23 +1,9 @@
--- phpMyAdmin SQL Dump
--- version 5.3.0-dev+20220611.3e6b0abbe2
--- https://www.phpmyadmin.net/
---
--- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th6 12, 2022 lúc 03:31 PM
--- Phiên bản máy phục vụ: 10.4.24-MariaDB
--- Phiên bản PHP: 8.1.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
 -- Cơ sở dữ liệu: `ecommerce`
 --
 DROP DATABASE IF EXISTS `ecommerce`;
@@ -260,13 +246,13 @@ ALTER TABLE `users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1000;
 
 INSERT INTO `users` (`id`, `fullName`, `email`, `dob`, `address`, `password`, `roleId`, `status`, `isConfirmed`, `phone`, `image`) VALUES
-('1000', 'Thanh Sang', 'admin@gmail.com', '2022-06-12', '276/1, Đường Tỉnh Lộ 827B', '123', 1, 1, 1, '0365840620', 'ic.jpg');
+('1000', 'Thanh Sang', 'admin@gmail.com', '2022-06-12', '276/1, Đường Tỉnh Lộ 827B', 'sang123', 1, 1, 1, '0365840620', 'ic.jpg');
 
 INSERT INTO `users` (`id`, `fullName`, `email`, `dob`, `address`, `password`, `roleId`, `status`, `isConfirmed`, `phone`,`image`) VALUES
-('1001', 'Nguyễn Tuấn Vinh', 'nguyentuanvinh1222@gmail.com', '2022-06-12', '276/1, Đường Tỉnh Lộ 827B', '12345', 1, 1, 1, '0793191854','');
+('1001', 'Nguyễn Tuấn Vinh', 'nguyentuanvinh1222@gmail.com', '2022-06-12', '276/1, Đường Tỉnh Lộ 827B', '123456', 1, 1, 1, '0793191854','');
 
 INSERT INTO `users` (`id`, `fullName`, `email`, `dob`, `address`, `password`, `roleId`, `status`, `isConfirmed`, `phone`,`image`) VALUES
-('1002', 'Thanh Sang', 'sang@gmail.com', '2022-06-13', '276/1, Đường Tỉnh Lộ 827B', '123', 2, 1, 1, '03456677','');
+('1002', 'Thanh Sang', 'sang@gmail.com', '2022-06-13', '276/1, Đường Tỉnh Lộ 827B', 'sang123', 2, 1, 1, '0345667789','');
 
 
 --
@@ -286,7 +272,12 @@ INSERT INTO `products` (`id`, `name`, `originalPrice`, `promotionPrice`, `image`
 (2001, 'Xe điều khiển 1:24 Ferrari FXXK Evo', '335000', '335000', 'R79300_1.jpg', 1000, '2022-03-08', 5001, 10, 'Xe điều khiển Ferrari FXXK Evo R79300 của thương hiệu RASTAR, được mua bản quyền và mô phỏng lại chính xác với tỷ lệ thu nhỏ 1:24. Sở hữu thiết kế chân thực như một chiếc xe ngoài đời thật, sản phẩm không chỉ là món đồ chơi giải trí sau những giờ học tập mệt mỏi mà còn giúp bé có thêm kiến thức về các loại phương tiện giao thông.', 1, 1),
 (2002, 'Trụ Sở Nghiên Cứu NASA Mặt Trăng', '3199000', '3199000', '60350_1_.jpg', 1000, '2022-03-11', 5002, 9, 'Hãy hạ cánh tàu đổ bộ mặt trăng để có những cuộc phiêu lưu tuyệt vời tại Trụ Sở Nghiên Cứu NASA Mặt Trăng. Tời và di chuyển các vật thể bằng máy bay không người lái, lấy mẫu từ máy bay VIPER và thực hiện các thí nghiệm trong phòng thí nghiệm khoa học và thực vật học. Sau đó nhảy lên xe buggy mặt trăng và đi đến khóa không khí và chỗ ở, nơi bạn có thể cất mũ bảo hiểm và ba lô của mình cho đến nhiệm vụ tiếp theo!', 1, 0),
 (2003, 'Xe Ford® F-150 Raptor', '4899000', '4899000', '42126_1_.jpg', 1000, '2022-03-11', 5003, 10, 'Xe bán tải Ford® nổi tiếng về sức mạnh và chức năng của chúng. Giờ đây, bạn có thể sở hữu sự bản sao của chiếc xe này với bộ LEGO® Technic ™ Xe Ford® F-150 Raptor (42126). Tận hưởng thời gian chất lượng khi bạn khám phá các tính năng thực tế được thiết kế trong mô hình giống như thật này, gồm động cơ V6 với các piston chuyển động, cùng với hệ thống treo trên tất cả các bánh xe.', 1, 0),
-(2004, 'Trạm Cứu Hỏa Abrick', '1000000', '1100000', '003026_2_.jpg', 1000, '2022-06-10', 5003, 20, 'Abrick ECOIFFIER 003026 Fire Station is a realistic fire station simulation toy model - a barracks complete with a team of qualified firefighters for all interventions: air, road… help put out quickly extinguish the fire, bringing peace to the people. Through simulation toys, children learn to understand the world around them, practice the skills they need for life, and be more independent every day.', 1, 2);
+(2004, 'Trạm Cứu Hỏa Abrick', '1100000', '1100000', '003026_2_.jpg', 1000, '2022-06-10', 5003, 20, 'Abrick ECOIFFIER 003026 Fire Station is a realistic fire station simulation toy model - a barracks complete with a team of qualified firefighters for all interventions: air, road… help put out quickly extinguish the fire, bringing peace to the people. Through simulation toys, children learn to understand the world around them, practice the skills they need for life, and be more independent every day.', 1, 2),
+(2005, 'Bộ lắp ráp bãi biển vui nhộn', '1400000', '1400000', 'Jr_123452.png', 1000, '2022-06-18', 5002, 20, 'Mùa hè thường gắn liền với những hoạt động vui chơi trên bãi biển đầy ắp tiếng cười. Tại bãi biển, các bé có thể trải nghiệm biết bao những hoạt động vui chơi trên bờ - dưới nước. Các bé có thể cùng bạn bè tổ chức tắm hồ bơi, làm một cửa hàng pha chế nước, nằm trải dài trên biển để đắp cát, xây những ngôi nhà cát. Hãy cùng Hptrade lắp ráp hoàn chỉnh Bộ lắp ráp Bãi biển vui nhộn cùng bạn bè.
+', 1, 2),
+(2006, 'Bộ lắp ráp robot siêu nhân', '900000', '900000', 'ask_a123231.png', 1000, '2022-06-10', 5002, 30, 'Bộ đồ chơi lắp ráp robot siêu nhân là người bạn cùng vui chơi mỗi ngày với bé thông qua việc lắp ráp và hóa thân vào các nhân vật trong mỗi sản phẩm. Đồ chơi thương hiệu Banbao có 20 dòng sản phẩm khác nhau với hơn 130 sản phẩm, chất liệu an toàn và những mẫu mã tính năng thông minh sẽ giúp bé vui chơi và phát triển những kĩ năng khám phá, vận động và thỏa sức sáng tạo.
+', 1, 10),
+(2007, 'Xe tăng điều khiển từ xa mini chính hãng Henglong M26 Pershing sóng 2.4Ghz, bắn đạn hồng ngoại, có giả lập âm thanh như thật', '1500000', '1500000', 'xetang_1231.png', 1000, '2022-06-10', 5001, 30, 'Xe tăng điều khiển từ xa Heng Long M26 Pershing là mẫu mini tank chính hãng vừa ra mắt của Henglong - thương hiệu xe tăng điều khiển từ xa đình đám trên thế giới với những sản phẩm có giá lên đến vài chục triệu đồng. M26 Pershing cùng M4A3 là mẫu xe tăng mini được phát triển riêng ở phân khúc giá bình dân, nhằm phổ cập xe tăng điều khiển đến mọi người chơi.', 1, 10);
 
 
 --
@@ -358,9 +349,6 @@ ALTER TABLE `users`
   ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`roleId`) REFERENCES `role` (`id`);
 COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 
 
 
